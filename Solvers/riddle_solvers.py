90a1f20f05e7135e47e29f65f161943152748612
mahmoudattia12 <es-mahmoudattia2025@alexu.edu.eg>
2024-03-05 18:59:09 +0200

    update sec_medium

--- Solvers/riddle_solvers.py
+++ Solvers/riddle_solvers.py
@@ -7,6 +7,10 @@ import numpy as np
 from SteganoGAN.utils import *
 from torchvision import transforms
 from PIL import Image
+# from sklearn.preprocessing import MinMaxScaler
+# from tensorflow.keras.models import Sequential
+# from tensorflow.keras.layers import LSTM, Dense
+# from sklearn.metrics import mean_squared_error
 
 
 def solve_cv_easy(test_case: tuple) -> list:
@@ -61,8 +65,84 @@ def solve_cv_hard(test_case: tuple) -> int:
     return 0
 
 
-def solve_ml_easy(data: pd.DataFrame) -> list:
-    data = pd.DataFrame(data)
+# def preprocess_data(data):
+#     scaler = MinMaxScaler(feature_range=(0, 1))
+#     scaled_data = scaler.fit_transform(data.values.reshape(-1, 1))
+#     return scaled_data, scaler
+#
+#
+# def create_dataset(data, look_back=1):
+#     X, Y = [], []
+#     for i in range(len(data) - look_back):
+#         X.append(data[i:(i + look_back), 0])
+#         Y.append(data[i + look_back, 0])
+#     return np.array(X), np.array(Y)
+#
+#
+# def build_lstm_model(look_back):
+#     model = Sequential()
+#     model.add(LSTM(50, input_shape=(look_back, 1)))
+#     model.add(Dense(1))
+#     model.compile(loss='mean_squared_error', optimizer='adam')
+#     return model
+
+
+# def solve_ml_easy(data: pd.DataFrame) -> list:
+#     # Preprocess data
+#     scaled_data, scaler = preprocess_data(data)
+#
+#     # Split data into train and test sets
+#     train_size = int(len(scaled_data) * 0.8)
+#     test_size = len(scaled_data) - train_size
+#     train, test = scaled_data[0:train_size, :], scaled_data[train_size:len(scaled_data), :]
+#
+#     # Reshape into X=t and Y=t+1
+#     look_back = 1
+#     X_train, Y_train = create_dataset(train, look_back)
+#     X_test, Y_test = create_dataset(test, look_back)
+#
+#     # Reshape input to be [samples, time steps, features]
+#     X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+#     X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+#
+#     # Build LSTM model
+#     model = build_lstm_model(look_back)
+#
+#     # Fit the model
+#     model.fit(X_train, Y_train, epochs=100, batch_size=1, verbose=2)
+#
+#     # Make predictions
+#     train_predict = model.predict(X_train)
+#     test_predict = model.predict(X_test)
+#
+#     # Invert predictions
+#     train_predict = scaler.inverse_transform(train_predict)
+#     Y_train = scaler.inverse_transform([Y_train])
+#     test_predict = scaler.inverse_transform(test_predict)
+#     Y_test = scaler.inverse_transform([Y_test])
+#
+#     # Calculate root mean squared error
+#     train_score = np.sqrt(mean_squared_error(Y_train[0], train_predict[:, 0]))
+#     test_score = np.sqrt(mean_squared_error(Y_test[0], test_predict[:, 0]))
+#     print('Train RMSE: %.2f' % (train_score))
+#     print('Test RMSE: %.2f' % (test_score))
+#
+#     # Forecast future attacks
+#     last_sequence = scaled_data[-look_back:]
+#     future_attacks = []
+#     for i in range(50):
+#         prediction = model.predict(np.reshape(last_sequence, (1, look_back, 1)))
+#         future_attacks.append(prediction[0][0])
+#         last_sequence = np.append(last_sequence[1:], prediction[0][0])
+#
+#     # Invert scaling for forecasted data
+#     future_attacks = np.array(future_attacks).reshape(-1, 1)
+#     future_attacks = scaler.inverse_transform(future_attacks)
+#
+#     return future_attacks.flatten().tolist()
+
+def solve_ml_easy(input: pd.DataFrame) -> list:
+    data = pd.DataFrame(input)
 
     """
     This function takes a pandas DataFrame as input and returns a list as output.
@@ -89,7 +169,7 @@ def solve_ml_medium(input: list) -> int:
     return 0
 
 
-def solve_sec_medium(input: Image) -> str:
+def solve_sec_medium(input) -> str:
     """
     This function takes an Image as input and returns a string as output.
 
@@ -99,7 +179,9 @@ def solve_sec_medium(input: Image) -> str:
     Returns:
     str: A string representing the decoded message from the image.
     """
-    image_tensor = image_to_tensor(input)
+    image_array = np.array(input, dtype=np.uint8)
+    pil_image = Image.fromarray(image_array, mode='RGB')
+    image_tensor = image_to_tensor(pil_image)
     return decode(image_tensor)
 
 
@@ -250,3 +332,10 @@ riddle_solvers = {
 path = "D://hackTrick//HackTrick24//SteganoGAN//sample_example//encoded.png"
 img = Image.open(path)
 print("Decoded Message:", solve_sec_medium(img))
+
+# # Load the CSV file
+# data = pd.read_csv("D://hackTrick//HackTrick24//Riddles//ml_easy_sample_example//series_data.csv")
+#
+# # Print the forecasted attacks
+# forecast = solve_ml_easy(data)
+# print(forecast)
